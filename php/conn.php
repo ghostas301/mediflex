2cf8722f4015b171e95147a8373c1a9b5abc0114
ghostas301 <44035902+ghostas301@users.noreply.github.com>
2024-12-07 23:46:28 +0100

    Update conn.php

--- php/conn.php
+++ php/conn.php
@@ -1,7 +1,15 @@
-<?php 
-$conn = new mysqli('sql309.infinityfree.com','if0_37856510','GXybtRjKfb','if0_37856510_domisoins');
+<?php
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_NAME');
+
+// Créer une connexion
+$conn = new mysqli($host, $user, $password, $dbname);
+
+// Vérifier la connexion
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
 ?>
