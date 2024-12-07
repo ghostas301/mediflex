7a9408ebf2579a996df9df61b867d19094afa049
ghostas301 <44035902+ghostas301@users.noreply.github.com>
2024-12-07 23:19:22 +0100

    Update conn.php

--- php/conn.php
+++ php/conn.php
@@ -1,3 +1,7 @@
 <?php 
 $conn = new mysqli('sql309.infinityfree.com','if0_37856510','GXybtRjKfb','if0_37856510_domisoins');
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
